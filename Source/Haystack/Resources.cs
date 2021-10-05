4f32628e3ad944cd8a96bde4414e993b7f504b26
Lisias T <me@lisias.net>
2021-10-05 03:20:12 -0300

    Code Sanitizing

--- Source/Haystack/Resources.cs
+++ Source/Haystack/Resources.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
 using UnityEngine;
-using ToolbarControl_NS;
 
 namespace Haystack
 {
--- Source/Haystack/StockSettings.cs
+++ Source/Haystack/StockSettings.cs
@@ -2,10 +2,7 @@
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using UnityEngine;
 
 
 namespace Haystack
