31e3ee4cc790c403be53ea81c3283c2cb8cf5623
Lisias T <me@lisias.net>
2021-10-30 11:26:35 -0300

    Legal mambo jambo

--- Source/Haystack/Properties/LegalMamboJambo.cs
+++ Source/Haystack/Properties/LegalMamboJambo.cs
@@ -3,7 +3,7 @@ namespace Haystack
 {
 	public static class LegalMamboJambo
 	{
-		public const string Company = "L Aerospace KSP Division";
+		public const string Company = "/L Aerospace KSP Division";
 		public const string Copyright = "© 2018-2021 LisiasT";
 		public const string Product = "Raystack";
 		public const string Trademark = "Raystack™ hermes-jr, enamelizer; Continued™ by Qberticus; ReContinued™ by LinuxGuguGames; /L Unleashed by LisiasT";
